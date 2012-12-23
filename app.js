fe0798ca78a6e3c5e71c6586da77fc160d6caf09
Alonso Robles <alonso@headspring.com>
2012-12-23 16:52:37 -0600

    Adding a basic API

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require('express');
 
 var app = express();
+app.listen(8000);
+
+var tweets = [];
 
 app.get('/', function(req, res) {
     res.send('Welcome to Node Twitter');
 });
 
-app.listen(8000);
+app.post('/send', express.bodyParser(), function(req, res) {
+   if (req.body && req.body.tweet) {
+       tweets.push(req.body.tweet);
+       res.send({status:"ok", message:"Tweet received"});
+   } else {
+       res.send({status:"nok", message:"No tweet received"});
+   }
+});
+
+app.get('/tweets', function(req, res) {
+   res.send(tweets);
+});
+
